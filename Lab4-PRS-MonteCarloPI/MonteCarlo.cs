bc6426708c76f5c91a62d957fa3bc34f4e84802c
radenkosetka7 <58879645+radenkosetka7@users.noreply.github.com>
2022-12-18 23:06:55 +0100

    Initial commit

---
+++ Lab4-PRS-MonteCarloPI/MonteCarlo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_PRS_MonteCarloPI
+{
+         class MonteCarlo
+        {
+        Random random = new Random();
+        public void PIIterations(int numOfIterations)
+        {
+            double pointX = 0;
+            double pointY = 0;
+            double radius = 0;
+            int circleCounter = 0;
+            int squareCounter = 0;
+            if(numOfIterations>0)
+            {
+                for(int i=0;i<numOfIterations;i++)
+                {
+                    pointX = random.NextDouble();
+                    pointY = random.NextDouble();
+                    radius = Math.Sqrt(pointX * pointX + pointY * pointY);
+                    if(radius<1)
+                    {
+                        circleCounter++;
+                        squareCounter++;
+                    }
+                    else
+                    {
+                        squareCounter++;
+                    }
+                }
+                double PI = (double)(4*(circleCounter/(double)squareCounter));
+                Console.WriteLine($"The estimated value of pi is {PI}. The real value of pi is {Math.PI}");
+                Console.WriteLine($"The difference between the programs calculation and the real value is {Math.PI - PI}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+        public void PIPrecision(int precision)
+        {
+            double pointX = 0;
+            double pointY = 0;
+            double radius = 0;
+            int circleCounter = 0;
+            int squareCounter = 0;
+            int numOfIterations = 0;
+            double difference=1/(Math.Pow(10,precision));
+            double PI = 0;
+            do
+            {
+                pointX = random.NextDouble();
+                pointY = random.NextDouble();
+                radius = Math.Sqrt(pointX * pointX + pointY * pointY);
+                if (radius < 1)
+                {
+                    circleCounter++;
+                    squareCounter++;
+                }
+                else
+                {
+                    squareCounter++;
+                }
+                PI = (double)(4*(circleCounter/(double)squareCounter));
+                numOfIterations++;
+            }
+            while (Math.Abs(PI - Math.PI) > difference);
+            Console.WriteLine($"The estimated value of pi is {PI}");
+
+        }
+    }
+}
---
+++ Lab4-PRS-MonteCarloPI/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_PRS_MonteCarloPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MonteCarlo monte = new MonteCarlo();
+            int input=0;
+            int precision=0;
+            int numOfIterations = 0;
+            try
+            {
+                Console.WriteLine("1. Number of iterations");
+                Console.WriteLine("2. Precision");
+                Console.WriteLine("0. End of program");
+                input = int.Parse(Console.ReadLine());
+                switch(input)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Insert the number of iterations:");
+                            numOfIterations = int.Parse(Console.ReadLine());
+                            monte.PIIterations(numOfIterations);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Insert the desired precision:");
+                            precision = int.Parse(Console.ReadLine());
+                            monte.PIPrecision(precision);
+                            break;
+                        }
+                    case 0:
+                        {
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Invalid input");
+                            break;
+                        }
+                }
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
